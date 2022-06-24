c95ebf5902041512cc0dc093b425857551df008d
Martin Toman <toman.martin@live.com>
2022-06-24 16:49:55 -0700

    Parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Expecting 1 argument: path to the transactions file.");
+        return;
+    }
+
+    let path = Path::new(&args[1]);
+    println!("{:?}", path);
 }
